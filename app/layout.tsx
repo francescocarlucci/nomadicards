bf6255868dcb20aecfb4679851d18fbd05a22227
Francesco Carlucci <info@francescocarlucci.com>
2025-04-10 11:14:47 +0200

    Updated meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Nomadicards",
+  title: "Nomadicards - Conversation Cards for Digital Nomads",
   description: "Meaningful Conversations for Digital Nomads",
 };
 
